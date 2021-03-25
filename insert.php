597c379d40745e4bfda830fd481f6d8585ca41ed
yonedaaan <79830771+yonedaaan@users.noreply.github.com>
2021-03-25 23:06:04 +0900

    SUKIMAJIKAN sample

---
+++ insert.php
@@ -0,0 +1,39 @@
+<?php
+//1. POSTデータ取得
+
+//まず前のphpからデーターを受け取る（この受け取ったデータをもとにbindValueと結びつけるため）
+$date = $_POST["date"];
+$time = $_POST["time"];
+$naiyou = $_POST["text"];
+
+//2. DB接続します xxxにDB名を入力する
+//ここから作成したDBに接続をしてデータを登録します xxxxに作成したデータベース名を書きます
+// mamppの方は
+// $pdo = new PDO('mysql:dbname=xxx;charset=utf8;host=localhost', 'root', 'root');
+
+try {
+    $pdo = new PDO('mysql:dbname=kadai4;charset=utf8;host=localhost', 'root', 'root');
+} catch (PDOException $e) {
+    exit('DbConnectError:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成 //ここにカラム名を入力する
+//xxx_table(テーブル名)はテーブル名を入力します
+$stmt = $pdo->prepare("INSERT INTO k_table(id,k_date, k_time, k_text)VALUES(NULL, :a1, :a2, :a3,");
+$stmt->bindValue(':a1', $date, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a2', $time, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a3', $text, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if ($status==false) {
+    //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+    $error = $stmt->errorInfo();
+    exit("QueryError:".$error[2]);
+} else {
+    //５．index.phpへリダイレクト 書くときにLocation: in この:　のあとは半角スペースがいるので注意！！
+    header("Location: index.php");
+    exit;
+}
+?>
